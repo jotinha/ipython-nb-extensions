d4ca07ada631cce17dca1489e2d7bf16a3ab9833
jotinha <jotinha@github>
2014-06-18 14:23:15 +0100

    searchhelp: code cleanup

--- searchhelp/main.js
+++ searchhelp/main.js
@@ -1,59 +1,65 @@
-var sendToPager = function(data) {
-  $([IPython.events]).trigger('open_with_text.Pager', data);
-};
-
-var sendHelpQuery = function(query) {
-  var callbacks = {
-    'shell' : {
-      'payload' : {
-        'page' : sendToPager            
-      },
-      reply: function(data) { 
-        //check if payload actually exists
-        if (data.content.payload.length === 0) {
-          //use this to handle invalid output!
-        }
-      },
-    }
+(function() {
+  var sendToPager = function(data) {
+    $([IPython.events]).trigger('open_with_text.Pager', data);
   };
-  IPython.notebook.kernel.execute(query + '?',callbacks,{
-    'silent':false,'store_history':false
-  });
-};
 
-var doSubmit = function() {
-  alert("hello");
-};
+  var sendHelpQuery = function(query) {
+    var callbacks = {
+      'shell' : {
+        'payload' : {
+          'page' : sendToPager            
+        },
+        reply: function(data) { 
+          //check if payload actually exists
+          if (data.content.payload.length === 0) {
+            //use this to handle invalid output!
+          }
+        },
+      }
+    };
+    IPython.notebook.kernel.execute(query + '?',callbacks,{
+      'silent':false,'store_history':false
+    });
+  };
 
-el = $(
-'<form class="navbar-search pull-right" id="helpSearch">'+
-'   <input type="search" class="search-query" placeholder="Search help" id="searchField">'+
-'</form>'
-);
+  var createSearchBox = function() {
 
-$('#maintoolbar-container').append(el);
+    el = $(
+    '<form class="navbar-search pull-right" id="helpSearch">'+
+    '   <input type="search" class="search-query" placeholder="Search help" id="searchField">'+
+    '</form>'
+    );
 
-$('#helpSearch').submit(function(event) {
-  var query = $(this).find('#searchField').val();
-  sendHelpQuery(query);
-  
-  event.preventDefault();
-});
+    $('#maintoolbar-container').append(el);
 
-//this handles the disabling/enabling of shortcuts on focus in/out
-IPython.keyboard_manager.register_events($('#helpSearch'));  
+    $('#helpSearch').submit(function(event) {
+      var query = $(this).find('#searchField').val();
+      sendHelpQuery(query);
+      
+      event.preventDefault();
+    });
 
-IPython.keyboard_manager.command_shortcuts.add_shortcuts({
-  'shift-H': {
-    handler: function(event) {
-      $('#searchField').focus();
-      return false;
+    //this handles the disabling/enabling of shortcuts on focus in/out
+    IPython.keyboard_manager.register_events($('#helpSearch'));  
 
-    }
-  }
+    IPython.keyboard_manager.command_shortcuts.add_shortcuts({
+      'shift-H': {
+        handler: function(event) {
+          $('#searchField').focus();
+          return false;
 
-})
+        }
+      }
+    })
+  };
 
-$('head').append('<link rel="stylesheet" href=' + 
-                  require.toUrl('custom/searchhelp/main.css') + 
-                  ' />');
+  var addCSS = function() {
+    $('head').append('<link rel="stylesheet" href=' + 
+                      require.toUrl('custom/searchhelp/main.css') + 
+                      ' />');
+  };
+
+  //addCSS();
+  createSearchBox();
+
+})();
