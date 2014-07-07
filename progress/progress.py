7fed0c5e117977b7d3aaed353d93988d9b65c56c
jotinha <jotinha@github>
2014-07-07 12:17:55 +0100

    progress.py using property

--- progress/progress.py
+++ progress/progress.py
@@ -7,18 +7,23 @@ Created on Fri Jul  4 15:40:10 2014
 
 from IPython.display import display,Javascript
 
-class NBProgress():
+class NBProgress(object):
+
     def __init__(self):
-        self.progress = 0.0
+        self._progress = 0.0    
+        
+    @property
+    def progress(self):
+        return self._progress
     
-    def add(self,delta):
-        self.progress += delta
+    @progress.setter
+    def progress(self,p):
+        p = 0 if p < 0 else 1 if p > 1 else p
+        self._progress = p
         self._send()
 
-    def set(self,prog):
-        self.progress = prog
-        self._send()
-
+        
     def _send(self):
+        #print "Sending " + str(self._progress)
         display('PROG',metadata={'progress':self.progress})
             
